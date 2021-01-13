6e45ec43ba05aa999d1727a962d169e34d65393d
KSH <ksh@ksh.com>
2021-01-12 20:20:25 -0600

    upd viewcsv

--- icse21/wd/scripts/viewcsv.sh
+++ icse21/wd/scripts/viewcsv.sh
@@ -2,7 +2,7 @@
 
 if ! [[ -z ${1:-} ]] &&  [[ -d "$1" ]]; then
     SRC_DIR="$(realpath "$(dirname "${BASH_SOURCE[0]}")")"
-    "$SRC_DIR/summary.sh" "$1" | "$SRC_DIR/viewcsv.sh"
+    "$SRC_DIR/summary.sh" "$@" | "$SRC_DIR/viewcsv.sh"
 else
     cat "${1:--}" | column -n -t -s, | less -S
 fi
